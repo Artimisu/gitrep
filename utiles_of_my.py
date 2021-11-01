ff3c8f089c580d71f5475497c0813f13c9f49cc7
dfq <1550633318@qq.com>
2021-11-01 21:25:47 +0800

    add utils of mine

---
+++ utiles_of_my.py
@@ -0,0 +1,31 @@
+import torch
+import numpy
+import ipdb
+import toch.nn as nn
+
+def load_image(image):
+
+
+def images_fuse(image1,image2):
+
+
+
+class Cnn_net(nn.Module):
+    def __init__(self):
+        super(Cnn_net,self).__init__()
+    self.conv2 = 
+
+    def forward(x):
+        x = 
+
+
+def show_image(image_tensor):
+
+
+def save_as_image(image_tensor):
+
+def show_feature(feature_tensor):
+
+
+def save_feature_as_image(feature_tensor):
+    
